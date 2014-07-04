1725cec01217775a1c95569a30e6e3926fb4feee
Allyen <allyen@seznam.cz>
2014-07-04 10:41:56 +0200

    Hack for a weird behavior of IE

--- src/Umbraco.Web/UI/JavaScript/Main.js
+++ src/Umbraco.Web/UI/JavaScript/Main.js
@@ -1,8 +1,24 @@
+var hackIntervalId = 0;
+
 LazyLoad.js("##JsInitialize##", function () {
     //we need to set the legacy UmbClientMgr path
+
+    // IE for some weird reason first runs this and then loads LegacyUmbClientMgr.js...
+    if (typeof UmbClientMgr === "undefined") {
+        hackIntervalId = setInterval(load, 5000);
+    } else {
+        load();
+    }
+});
+
+function load() {
+    if (hackIntervalId != 0) {
+        clearInterval(hackIntervalId);
+    }
+
     UmbClientMgr.setUmbracoPath('"##UmbracoPath##"');
 
     jQuery(document).ready(function () {
         angular.bootstrap(document, ['umbraco']);
     });
-});
+}
