c31d1fccae304cc0a7e76cda3ff5694a65bc4c55
Allyen <allyen@seznam.cz>
2014-11-24 16:59:50 +0100

    Up-to-date debug build

--- build/Build-debug.bat
+++ build/Build-debug.bat
@@ -1,6 +1,15 @@
 @ECHO OFF
-SET /p release=<UmbracoVersion.txt
-SET comment=
+IF NOT EXIST UmbracoVersion.txt (
+	ECHO UmbracoVersion.txt missing!
+	GOTO :showerror
+)
+
+REM Get the version and comment from UmbracoVersion.txt lines 2 and 3
+SET "release="
+SET "comment="
+FOR /F "skip=1 delims=" %%i IN (UmbracoVersion.txt) DO IF NOT DEFINED release SET "release=%%i"
+FOR /F "skip=2 delims=" %%i IN (UmbracoVersion.txt) DO IF NOT DEFINED comment SET "comment=%%i"
+
 SET version=%release%
 
 IF [%comment%] EQU [] (SET version=%release%) ELSE (SET version=%release%-%comment%)
@@ -8,12 +17,30 @@ ECHO Building Umbraco %version%
 
 ReplaceIISExpressPortNumber.exe ..\src\Umbraco.Web.UI\Umbraco.Web.UI.csproj %release%
 
-ECHO Installing the Microsoft.Bcl.Build package before anything else, otherwise you'd have to run build.cmd twice
+ECHO Installing the Microsoft.Bcl.Build package before anything else, otherwise you'd have to run build.cmd twice  
 SET nuGetFolder=%CD%\..\src\packages\
+..\src\.nuget\NuGet.exe sources Remove -Name MyGetUmbracoCore
+..\src\.nuget\NuGet.exe sources Add -Name MyGetUmbracoCore -Source https://www.myget.org/F/umbracocore/api/v2/ >NUL
 ..\src\.nuget\NuGet.exe install ..\src\Umbraco.Web.UI\packages.config -OutputDirectory %nuGetFolder%
+..\src\.nuget\NuGet.exe install ..\src\umbraco.businesslogic\packages.config -OutputDirectory %nuGetFolder%
+..\src\.nuget\NuGet.exe install ..\src\Umbraco.Core\packages.config -OutputDirectory %nuGetFolder%
 
-ECHO Performing MSBuild and producing Umbraco binaries, BELLE BUILD EXCLUDED!!! To build belle, Umbraco.Web.UI.csproj must be clean and rebuilt (change in build-debug.proj)
+ECHO Removing the belle build folder to make sure everything is clean as a whistle
+RD ..\src\Umbraco.Web.UI.Client\build /Q /S
+
+ECHO Removing existing built files to make sure everything is clean as a whistle
+RMDIR /Q /S _BuildOutput
+DEL /F /Q UmbracoCms.*.zip
+DEL /F /Q UmbracoExamine.*.zip
+DEL /F /Q UmbracoCms.*.nupkg
+DEL /F /Q webpihash.txt
+
+ECHO Making sure Git is in the path so that the build can succeed
+CALL InstallGit.cmd
+ECHO Performing MSBuild and producing Umbraco binaries zip files
 %windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "Build-debug.proj" /p:BUILD_RELEASE=%release% /p:BUILD_COMMENT=%comment%
+
+IF ERRORLEVEL 1 GOTO :showerror
 
 echo off
 SET SRC=c:\Repositories\Umbraco\build\_BuildOutput\WebApp
@@ -26,4 +53,7 @@ robocopy %SRC%\umbraco %DEST%\umbraco /S /XO /IS /NJH /NFL /NDL
 echo. && echo umbraco_client:
 robocopy %SRC%\umbraco_client %DEST%\umbraco_client /S /XO /IS /NJH /NFL /NDL
 
-REM pause
+GOTO :EOF
+
+:showerror
+PAUSE
