d438b0c9edc5119ff74e4c6216adafb6c3306f72
Allyen <allyen@seznam.cz>
2015-03-20 22:32:17 +0100

    Macro properties in sort order

--- src/Umbraco.Web/Models/ContentEditing/MacroParameter.cs
+++ src/Umbraco.Web/Models/ContentEditing/MacroParameter.cs
@@ -20,6 +20,9 @@ namespace Umbraco.Web.Models.ContentEditing
 
         [DataMember(Name = "name")]
         public string Name { get; set; }
+
+        [DataMember(Name = "sortOrder")]
+        public string SortOrder { get; set; }
         
         /// <summary>
         /// The editor view to render for this parameter
