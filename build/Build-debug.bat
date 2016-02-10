30c71df61ee093d7f4c58434c2220bd8f7988581
Pavel Budik <budik@ics.muni.cz>
2016-02-10 19:00:35 +0100

    Debug build dest dir fix

--- build/Build-debug.bat
+++ build/Build-debug.bat
@@ -45,12 +45,12 @@ IF ERRORLEVEL 1 GOTO :showerror
 IF "%1"=="" GOTO :EOF
 
 echo off
-SET SRC=c:\Repositories\Umbraco\build\_BuildOutput_Debug
+SET SRC=c:\Repositories\Umbraco-CMS\build\_BuildOutput_Debug
 SET DEST=n:\web\webcentrum2-dev.muni.cz\%1
 
 echo. && echo bin:
 robocopy %SRC%\WebApp\bin %DEST%\lib\Umbraco-CMS\src\build\_BuildOutput_Debug\bin /S /XO /IS /NJH /NFL /NDL
-robocopy %SRC%\UmbracoExamine.PDF %DEST%\lib\Umbraco-CMS\src\build\_BuildOutput_Debug\bin /S /XO /IS /NJH /NFL /NDL
+REM robocopy %SRC%\UmbracoExamine.PDF %DEST%\lib\Umbraco-CMS\src\build\_BuildOutput_Debug\bin /S /XO /IS /NJH /NFL /NDL
 echo. && echo umbraco:
 robocopy %SRC%\WebApp\umbraco %DEST%\web\Redakce /S /XO /IS /NJH /NFL /NDL
 echo. && echo umbraco_client:
